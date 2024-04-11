2e64b987f04dc64ba4a2dafb6992890ac7d23e84
FabioDiCeglie <fabiod@backbase.com>
2024-04-11 15:57:35 +0200

    chore: added small console.log

--- server-nodeJS/index.js
+++ server-nodeJS/index.js
@@ -44,6 +44,7 @@ const PORT = process.env.PORT || 3000;
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
+    console.log('Connected to MongoDB')
     app.listen(PORT, () => {
       console.log(`Server is running on http://localhost:${PORT}`);
     });
