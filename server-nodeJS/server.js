b80e686347bee2a7b7848801490537aa250ca7bd
FabioDiCeglie <fabiod@backbase.com>
2024-04-11 10:50:02 +0200

    feat: introducing mongoose and security features

--- server-nodeJS/server.js
+++ server-nodeJS/server.js
@@ -3,14 +3,26 @@ import express from 'express';
 import { posts } from './utils/fixtures.js';
 import { router as user } from './routes/user/auth.js';
 import { authenticateToken } from './utils/helpers.js';
+import helmet, { crossOriginResourcePolicy } from 'helmet';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
 app.use(cors());
+
+// This line of code is a security middleware that helps protect the application
+// from common vulnerabilities such as cross-site scripting (XSS), content sniffing, clickjacking, etc.
+app.use(helmet());
+
+// This line of code sets a Cross-Origin Resource Policy (CORP) header to allow cross-origin requests.
+// This middleware helps to prevent attackers
+// from exploiting vulnerabilities in the application by sending malicious requests from other domains.
+app.use(crossOriginResourcePolicy({ policy: 'cross-origin' }));
 
 app.get('/', (req, res) => {
   res.json([
@@ -29,6 +41,14 @@ app.use('/auth', user);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+
+    // /* ADD DATA ONE TIME */
+    // User.insertMany(users);
+  })
+  .catch((error) => console.log(`${error} did not connect`));
